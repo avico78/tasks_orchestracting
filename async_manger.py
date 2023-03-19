ff661b333d10fc065292bcb9360d228cba1964ca
Avi Cohen <avico78@gmail.com>
2023-03-19 11:10:41 +0200

    new

--- async_manger.py
+++ async_manger.py
@@ -11,17 +11,18 @@ import asyncio
 class Manager:
 
     """
-    Manager has dedciated exhcange and queue "customer_<#>" ,
-    Where exhange bind with tasks by the task_uuid .
-    
-    And each Task can publish to this queue with currernt state with it's uuid.
-    So Manager can idetify what current Task status (error,success).
-        
-    if any tasks publish "NOT_OK" to Manager exchange -> Manager send rollback to all tasks at once,
-    
+    Manager has dedciated exhcange and queue ,
+    Where exhange bind only to relavant tasks  by task_uuid .
+
+    Tasks will publish message with routing_key contain task_uuid for it state .
+    So Manager can idetify what current Task status (error,success)
+
+    Once all Tasks sent messages - Customer can decide for next step OR
+    Base on "FAILED_ALL_ON_FIRST_ERROR" - it can immidetely notify all tasks to 
+    rollback (endcase scenrio , Task should aborted if not started)
     
     """
-
+    # FAILED_ALL_ON_FIRST_ERROR:bool = True
 
     def __init__(self, exchange_name, queue_name: str, connection_string: str = None, routing_keys: list = None):
         self.exchange_name = exchange_name
@@ -31,14 +32,14 @@ class Manager:
         self.exchange = None
         self.channel = None
         self.queue = None
-        self.routing_keys: ["tasks_uuid"] = list(map(str,routing_keys))
+        self.routing_keys: ["task_rule_ids"] = list(map(str,routing_keys))
         self.tasks_state = None
 
     async def connect(self):
         self.connection = await aio_pika.connect_robust(self.connection_string)
         self.channel = await self.connection.channel()
         await self.channel.set_qos(prefetch_count=10)
-        
+
         self.exchange = await self.channel.declare_exchange(
            self.exchange_name, ExchangeType.DIRECT,auto_delete=False
         )
@@ -51,8 +52,14 @@ class Manager:
         else:
             print(f"The queue '{self.queue_name}' exists and empty")        
 
+        # creating queue per task with auto_delete
+        # seem safer having the task queue predefine before executing the task
+        for task_queue in self.routing_keys:
+             await self.channel.declare_queue(task_queue, auto_delete=True)
+            
 
         await self._bind()
+        
 
          
     async def consume(self):
@@ -61,34 +68,36 @@ class Manager:
 
         """
         print("Start consuming!")
+        print(*self.routing_keys, sep = "\n")
         messages_to_consume = self.routing_keys.copy()
-
-        async with self.queue.iterator() as queue_iter:
+        async with self.queue.iterator() as queue_iter:           
             message: AbstractIncomingMessage
             async for message in queue_iter:
-                async with message.process():   
-                       if str(message.routing_key) in messages_to_consume:
-                            print(message.body.decode())
-                            data = message.body.decode()
-                            data_json = json.loads(data)
-                   
-                            if data_json["status"] == "NOT_OK":
-                                await self.rollback_tasks()
-                            messages_to_consume.remove(str(message.routing_key))
+                async with message.process(ignore_processed=False):   
+                        print("received messaged from task ", message.body.decode())
+                        data = message.body.decode()
+                        data_json = json.loads(data)
+                        
+                        if data_json["status"] in ["code_1","code_2"]:
+                            print("Rollback required!!!")
+                            await self.rollback_tasks()
+                            return
+                        messages_to_consume.remove(str(message.routing_key))
 
-                            if len(messages_to_consume) == 0:
-                                await self.commit_tasks()
-                                break
-
+                        if len(messages_to_consume) == 0:
+                            await self.commit_tasks()
+                            break
+          
 
     async def _bind(self):
-        # binding all routing kets to Manager exhange
-        for task_uuid in self.routing_keys:    
-                await self.queue.bind(self.exchange, routing_key=task_uuid)
+        for routing_key in self.routing_keys:    
+                await self.queue.bind(self.exchange, routing_key=routing_key)
 
     async def close(self):
+        print("Closing connection")
         if self.connection is not None:
             await self.connection.close()
+            
 
     async def run(self):
         await self.connect()
@@ -98,39 +107,40 @@ class Manager:
 
     async def publish(self,queue_name,message_body):
         print(f'publishing:{message_body} for {queue_name}')
-        queue = await self.channel.declare_queue(queue_name)
         message = aio_pika.Message(body=message_body.encode("utf-8"))
         await self.channel.default_exchange.publish(
                 aio_pika.Message(body=message),
                 routing_key=queue_name
             )
+ 
 
     async def rollback_tasks(self):
-        
-            for task_uuid in self.routing_keys:
-                await self.publish(queue_name=str(task_uuid),message_body="rollback")
+            """
+            TODO:
+            Add support for updating the tasks only after all tasks messages 
+            recivied(all tasks on final stage-commit/rollback)
+            """
+            for routing_key in self.routing_keys:
+                await self.publish(queue_name=routing_key, message_body="rollback",)
             
-            exit()
+            return
 
     async def commit_tasks(self):
-            for task_uuid in self.routing_keys:
-                await self.publish(queue_name=str(task_uuid),message_body="commit")
+            for routing_key in self.routing_keys:
+                await self.publish(queue_name=routing_key, message_body="commit")
 
-            exit()
+            return
 
 async def main():
-# for demo , will inititate the Manager by 100 tasks 
-# Which mean the Manager exchange associate by 100 routing_keys (in real world each tasks has it's own uuid)
-
-    number_of_tasks=100
-    routing_keys = list(map(str,range(1,number_of_tasks)))
-    consumer = Manager(queue_name="customer_1",exchange_name="customer_1", connection_string="amqp://guest:guest@localhost/",routing_keys=routing_keys)
+    queue = 'customer_1'
+    number_of_task = 3
+    task_rule_ids = range(1,number_of_task)
+    routing_keys = [f'task_1_' + str(id)  for id in task_rule_ids]
+    consumer = Manager(queue_name=queue, exchange_name=queue, connection_string="amqp://guest:guest@localhost/",routing_keys=routing_keys)
     await consumer.run()
 
 
+
 asyncio.run(main())
-
-
-
 
 
--- task.py
+++ task.py
@@ -1,67 +1,27 @@
 import pika
-
+import time
+from random import randint,choice
 
 class TaskMsg:
-  
-  """
-  
-  Task actually instantiate inside Celery task - so not sure if should use asyncio ,
-  
-  Task push message to Manager exchange and update Manager with current status .
-  Task consume message from it's own queue (queueu=task_uuid) - where  Manager can push message and notify task for next step(commit,rollback)
-  based on what he recvied on Manager queue (it idetify by routing_key = task_uuid).
-
-  In Celery it looks something like:
-  
-  
-  @app.task(bind=True ,base=RuleBase,  name='proccess_rule')
-  def proccess_rule(self, *args, **kwargs):
-
-    main_id = kwargs.get('main_id')
-    rule_uuid = kwargs.get('rule_uuid')
-    source_type = kwargs.get('source_type')
-    source_name = kwargs.get('source_name')
-    ...
-    ...
-    from sqlalchemy.exc import SQLAlchemyError
-    
-      with TaskMsgBroker(connection_string=connection_string, queue_consumer=rule_uuid ,exchange_name='customer_1' ,routing_key=rule_uuid) as task_manager:
-      
-        if source_type == 'db':
-          db_connection = self.db[source_name]
-        load_operation.df_to_table(conn=db_connection, table_name=target_object_name ,df=df ,if_exists='append')
-        try:
-            db_connection.commit
-            message['status'] = "OK"
-            task_manager.produce(message=json.dumps(message))
-            task_manager.consume(callback)          
-        except SQLAlchemyError:
-            message['status'] = "NOT_OK"
-            task_manager.produce(message=json.dumps(message))
-            task_manager.consume(callback)
-  
-  
-  
-  
-  """
-
-  
-    def __init__(self, connection_string: str = None, queue_consumer: str = None ,exchange_name:str = None ,routing_key:str = None):
+    def __init__(self, connection_string: str = None, queue_task: str = None ,exchange_name:str = None ,routing_key:str = None):
         self.connection_string = connection_string or f'amqp://guest:guest@localhost:5672/'
-        self.queue_consumer = queue_consumer
+        self.queue_task = queue_task
         self.exchange_name = exchange_name
         self.routing_key = routing_key
     def __enter__(self):
         self.connection = pika.BlockingConnection(pika.URLParameters(self.connection_string))
         self.channel = self.connection.channel()
-        self.channel.queue_declare(queue=self.queue_consumer)
+     #   self.channel.queue_declare(queue=self.queue_task)
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
+        print("Exiting",self.routing_key)
+       # self.channel.queue_delete(self.queue_task)   
         self.connection.close()
 
     def consume(self, callback):
-        self.channel.basic_consume(queue=self.queue_consumer, on_message_callback=callback, auto_ack=True)
+        self.channel.basic_consume(queue=self.queue_task, on_message_callback=callback)
+        #self.channel.basic_ack()
         self.channel.start_consuming()
 
     def produce(self ,message):
@@ -75,32 +35,51 @@ class TaskMsg:
 
 
 
-def callback(ch, method, properties, body):
+def callback(channel, method_frame, header_frame, body):
     data = body.decode()
+    print(method_frame.delivery_tag)
+
     print("Manager decision:", data)
     if data == "commit":
         print("Got it ,commit the data")
     elif data == "rollback":
         print("Got it ,rollback preform")
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)        
     exit()
 
 
+class TestFailed(Exception):
+    
+    def __init__(self,message:str):
+        self.message = message
+        super().__init__(message)
+    def __str__(self):
+        return f'Error: {self.message}'
+    
+
+
 def run_task(i:int):
-        
+
         connection_string = 'amqp://guest:guest@localhost:5672/'
         queue = 'customer_1'
-        rules_uuid = list(map(str,range(1,number_of_tasks)))
-        message = {'rule_id':1, 'main_id':1, 'rule_uuid':rules_uuid[i], 'status': None}
+        rules_id = list(map(str,range(1,number_of_tasks)))
 
-        with TaskMsg(connection_string=connection_string, queue_consumer=rules_uuid[i] ,exchange_name=queue ,routing_key=rules_uuid[i]) as task:
-            try:
-                5/(50-i)
+        message = {'rule_id':1, 'main_id':1, 'rule_uuid':f'task_1_{i}' , 'status': None}
+        print("creating task",message['rule_uuid'])
+        with TaskMsg(connection_string=connection_string, queue_task=f'task_1_{i}' ,exchange_name=queue ,routing_key=f'task_1_{i}') as task:
+
+            try:            
+               # time.sleep(randint(1,3))    
+                if i == 2:
+                    print("Test - Failing Task #",i)
+                    raise TestFailed(choice(["code_1","code_2"]))
+                
                 message['status'] = "OK"
-                print(message)
                 task.produce(message=json.dumps(message))
                 task.consume(callback)
-            except ZeroDivisionError:
-                message['status'] = "NOT_OK"
+            except TestFailed as e:
+                time.sleep(randint(1,5))
+                message['status'] = e.message
                 task.produce(message=json.dumps(message))
                 task.consume(callback)
 
@@ -110,17 +89,16 @@ if __name__ == '__main__':
 
     import json
     import threading
-    number_of_tasks=100
+    number_of_tasks=3
     
     threads = []
     
-    for i in range(0,number_of_tasks):
+    for i in range(1,number_of_tasks):
         thread = threading.Thread(target=run_task, args=(i,))
         threads.append(thread)        
 
     for j in threads:
         j.start()
-
 
     for j in threads:
         j.join()
